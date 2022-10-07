1a5beb3de35f92c68ce67c36e2db770a00b2100b
yogxat <yogxat@xoth.com>
2022-10-07 01:05:38 -0500

    .

--- client/src/components/home/main.tsx
+++ client/src/components/home/main.tsx
@@ -4,9 +4,6 @@ export function Brand() {
         <main className="px-4 text-center">
             <h1>Fanke TS</h1>
             <p className="lead px-4">Este es mi primer proyecto serio, si se puede decir, de desarrollo web. Mi versión de la aplicación de estudio Anki</p>
-            <p className="lead">
-                <button className='btn btn-primary'>About</button>
-            </p>
         </main>
     )
 }
